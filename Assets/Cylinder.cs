2928a62df746554d24aa944782a1d9fcd8875c1f
HyeonggiMin <167495767+HyeonggiMin@users.noreply.github.com>
2024-04-25 17:22:38 +0900

    feat: 물체 이동 씬 구현
    - Conveyor, Cylinder Object Unit 구현
    - Cylinder 업그레이드
    - Times 구현

---
+++ Assets/Cylinder.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 시작시 Player가 뒤 방향으로 이동한다.
+public class Cylinder : MonoBehaviour
+{
+    public float speed = 2;
+    public Transform destination;
+    public float distanceLimit = 0.3f;
+    public Timer timer;
+    float arrivalTime;
+    bool isArrived = false;
+    void Start()
+    {
+
+    }
+
+
+    void Update() // 프레임이 갱신될 때 실행되는 매서드 0.002 ~ 0.004에 한번씩 이동
+    {
+        if (!isArrived)
+        {
+            Vector3 direction = Vector3.back;
+
+            // 현 위치에서부터 destination까지의 벡터
+            Vector3 dir2Dest = (destination.position - transform.position).normalized;
+            float distance = (destination.position - transform.position).magnitude;
+
+            if (distance > distanceLimit)
+            {
+                transform.position += dir2Dest * Time.deltaTime * speed;
+            }
+            else
+            {
+                isArrived = true;
+
+                // 도착 시 알림
+                arrivalTime = timer.currentTime;
+                print("도착시간: " + arrivalTime);
+            }
+        }
+    }
+
+    // 충돌이 시작되었을 때 실행되는 함수
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Object"))
+        {
+            print(collision.gameObject.name + "에 충돌했습니다.");
+        }
+    }
+
+    // 충돌이 진행 중 실행되는 함수
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Object"))
+        {
+            print(collision.gameObject.name + "에 붙어있습니다.");
+        }
+    }
+
+    // 충돌이 끝났을 때 실행되는 함수
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Object"))
+        {
+            print(collision.gameObject.name + " 충돌이 끝났습니다.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Object"))
+        {
+            print("OnTriggerEnter");
+        }
+    }
+}
+
---
+++ Assets/Timer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 시간의 경과를 특정 변수에 저장한다.
+public class Timer : MonoBehaviour
+{
+    public float currentTime = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        //print("경과 시간: " + currentTime);
+    }
+}
